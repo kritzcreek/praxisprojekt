a2d8727a18b2129d5fc95c835d69c55b7aa673b7
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2016-04-13 14:56:32 +0200

    simpler Haskell Client

--- haskell-producer/producer/Main.hs
+++ haskell-producer/producer/Main.hs
@@ -1,71 +1,54 @@
 module Main where
 
+import Control.Monad
+import Control.Concurrent (threadDelay, forkIO)
 import Haskakafka
 import qualified Data.ByteString.Char8 as C8
+import System.Exit (exitSuccess)
+
+kafkaConfig :: [(String, String)]
+kafkaConfig = [("socket.timeout.ms", "50000")]
+
+topicConfig :: [(String, String)]
+topicConfig = [("request.timeout.ms", "50000")]
+
+connectionString :: String
+connectionString = "192.168.99.100:9092"
+
+partition :: Int
+partition = 0
 
 main :: IO ()
 main = do
-  let
-      -- Optionally, we can configure certain parameters for Kafka
-      kafkaConfig = [("socket.timeout.ms", "50000")]
-      topicConfig = [("request.timeout.ms", "50000")]
+  -- Consumer der alle messages auf stdout ausgibt
+  _ <- forkIO (loggingConsumer "topic")
+  -- Nimmt solange Messages an, bis "quit" eingegeben wird
+  withKafkaProducer kafkaConfig topicConfig connectionString "topic" $
+    \_ topic -> handleInput topic
+  -- Bricht den log thread ab
+  exitSuccess
 
-      -- Payloads are just ByteStrings
-      samplePayload = C8.pack "Hello world"
+handleInput :: KafkaTopic -> IO ()
+handleInput topic = do
+  input <- C8.getLine
+  if C8.unpack input == "quit"
+    then pure ()
+    else produceMessage topic (KafkaSpecifiedPartition partition) (KafkaProduceMessage input)
+         >> handleInput topic
 
-
-  -- withKafkaProducer opens a producer connection and gives us
-  -- two objects for subsequent use.
-  withKafkaProducer kafkaConfig topicConfig
-                    "192.168.99.100:9092" "topic"
+loggingConsumer :: String -> IO ()
+loggingConsumer topicString =
+  withKafkaConsumer kafkaConfig topicConfig
+                    connectionString topicString
+                    partition -- locked to a specific partition for each consumer
+                    KafkaOffsetEnd -- start reading from beginning (alternatively, use
+                                        -- KafkaOffsetEnd, KafkaOffset or KafkaOffsetStored)
                     $ \kafka topic -> do
-
-    -- Produce a single unkeyed message to partition 0
-    let message = KafkaProduceMessage samplePayload
-    _ <- produceMessage topic (KafkaSpecifiedPartition 0) message
-    pure ()
-
-    -- -- Produce a single keyed message
-    -- let keyMessage = KafkaProduceKeyedMessage (C8.pack "Key") samplePayload
-    -- _ <- produceKeyedMessage topic keyMessage
-
-    -- -- We can also use the batch API for better performance
-    -- _ <- produceMessageBatch topic KafkaUnassignedPartition [message, keyMessage]
-
-    -- putStrLn "Done producing messages, here was our config: "
-    -- dumpConfFromKafka kafka >>= \d -> putStrLn $ "Kafka config: " ++ (show d)
-    -- dumpConfFromKafkaTopic topic >>= \d -> putStrLn $ "Topic config: " ++ (show d)
-
-
-  -- withKafkaConsumer opens a consumer connection and starts consuming
-  -- let partition = 0
-  -- withKafkaConsumer kafkaConfig topicConfig
-  --                   "localhost:9092" "topic"
-  --                   partition -- locked to a specific partition for each consumer
-  --                   KafkaOffsetBeginning -- start reading from beginning (alternatively, use
-  --                                        -- KafkaOffsetEnd, KafkaOffset or KafkaOffsetStored)
-  --                   $ \kafka topic -> do
-  --   -- Consume a single message at a time
-  --   let timeoutMs = 1000
-  --   me <- consumeMessage topic partition timeoutMs
-  --   case me of
-  --     (Left err) -> putStrLn $ "Uh oh, an error! " ++ (show err)
-  --     (Right m) -> putStrLn $ "Woo, payload was " ++ (C8.unpack $ messagePayload m)
-
-  --   -- For better performance, consume in batches
-  --   let maxMessages = 10
-  --   mes <- consumeMessageBatch topic partition timeoutMs maxMessages
-  --   case mes of
-  --     (Left err) -> putStrLn $ "Something went wrong in batch consume! " ++ (show err)
-  --     (Right ms) -> putStrLn $ "Woohoo, we got " ++ (show $ length ms) ++ " messages"
-
-
-  --   -- Be a little less noisy
-  --   setLogLevel kafka KafkaLogCrit
-
-  -- -- we can also fetch metadata about our Kafka infrastructure
-  -- let timeoutMs = 1000
-  -- emd <- fetchBrokerMetadata [] "localhost:9092" timeoutMs
-  -- case emd of
-  --   (Left err) -> putStrLn $ "Uh oh, error time: " ++ (show err)
-  --   (Right md) -> putStrLn $ "Kafka metadata: " ++ (show md)
+    setLogLevel kafka KafkaLogCrit
+    forever (threadDelay 1000000 >> consumeSingle topic)
+  where
+  consumeSingle :: KafkaTopic -> IO ()
+  consumeSingle topic =
+    consumeMessage topic partition 1000 >>= either
+      (const (pure ()))
+      (C8.putStrLn . (C8.pack "Message: " `C8.append`) . messagePayload)
